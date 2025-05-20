1fbf4e0629ac3a048bd810b26a34faf69578e3d1
Caroline Meireles <carolinemeireles.si@gmail.com>
2025-05-20 11:07:00 -0300

    criacao da barra lateral

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eee;
+  height: 100vh;
+`
+
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 8px;
+  margin-top: 16px;
+`
+export const Campo = styled.input`
+  border: 2px solid #666;
+  border-radius: 8px;
+  padding: 8px;
+  background-color: #fff;
+  font-weight: bold;
+  width: 100%;
+`
